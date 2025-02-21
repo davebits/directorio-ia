5d7b6bb25e86393ae5b20fbd494f36c7e6aed918
David Alejandro Suarez <alejo7682@gmail.com>
2025-02-21 14:12:25 -0500

    Primer commit: Estructura inicial del proyecto

---
+++ src/main/java/com/miapp/directorio_ia/DirectorioIaApplication.java
@@ -0,0 +1,13 @@
+package com.miapp.directorio_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorioIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorioIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miapp/directorio_ia/DirectorioIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miapp.directorio_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorioIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
